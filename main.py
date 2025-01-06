57db218c480aff49d148aab35d5a02a42e60faab
Patryk Marczak <m.patryk@outlook.com>
2025-01-06 16:06:55 +0100

    adds basic data analysis

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello, World!")
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+important_columns = ['DEPRESSIONINDEX', 'DEPEPISODE', 'MDELASTYR', 'ANYTXRXMDE', 'CATAG2', 'CATAG3', 'CATAG7', 'IRSEX', 'NEWRACE2', 'ANYINDEX', 'MJANDCOKE', 'ILLICITDRUGUSE', 'LSYRILLICIT', 'COKECRACK', 'OTHERILLICIT', 'MARJLTYR', 'MJCOKELY', 'COCCRKLY', 'MJGT12MO', 'COCGT12MO', 'ANYGT12MO', 'ALCFMFPB', 'IREDUC2', 'EDU_DUMMY', 'INCOME', 'INCOME_R', 'POVERTY', 'IRPRVHLT', 'WORKFORCE', 'EMPSTAT4', 'REVERSEPOP', 'MOVESPY2', 'CACHAR', 'CATYPE', 'CRIMEHIST', 'ANYSDRUG', 'ANYATTACK', 'ANYTHEFT', 'NUMARREST', 'HEALTH2', 'SCHDSICK', 'SCHDSKIP', 'TXLCAD', 'DSTNCALM', 'DSTTIRE', 'DSTSITST', 'DSTDEPRS', 'DSTCHEER', 'DSTNRVOS', 'IRMARIT', 'NOMARR2', 'RKIDSHH', 'MARRIED', 'CHILDRENINHOME']
+
+def main():
+    df = pd.read_csv('./data/27521-0001-Data.tsv', sep='\t')
+    df_age_analysis = df[important_columns]
+
+    corrmat = df_age_analysis.corr()
+    corrmat_high = corrmat[abs(corrmat['DEPRESSIONINDEX']) > 0.7]
+    f, ax = plt.subplots(figsize=(12, 9))
+    sns.heatmap(corrmat_high, square=True)
+    plt.show()
 
 if __name__ == "__main__":
     main()
